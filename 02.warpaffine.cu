5cd9dcacce0c1cc6005bba757753188ac6a71875
whitesnowyyy <120913574+whitesnowyyy@users.noreply.github.com>
2023-04-27 10:46:45 +0800

    Add files via upload

---
+++ 02.warpaffine.cu
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// #include <cuda_runtime.h>
+
+// // compute最终会在gpu上，以3个线程启动进行执行
+// __global__ void warpAffine_kernel(unsigned char* src, unsigned char* dst, float* M){
+
+//     int position = (blockDim.x * blockIdx.x + threadIdx.x);
+
+
+
+// }
